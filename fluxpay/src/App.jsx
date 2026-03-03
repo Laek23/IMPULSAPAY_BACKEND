d989a38053269a250983b1cdcfbb547994893912
alexander <castillobarbudoalexander@gmail>
2026-03-02 18:14:18 -0600

    nuevo rutas

--- fluxpay/src/App.jsx
+++ fluxpay/src/App.jsx
@@ -3,16 +3,30 @@ import Login from "./Login";
 import Register from "./Register";
 import Dashboard from "./Cliente/Dashboard";
 import ClienteTarjeta from "./Cliente/ClienteTarjeta";
+
 import DashboardAdmin from "./Administrador/Dashboard";
+import GestionNegocios from "./Administrador/GestionNegocios";
+import AgregarNegocio from "./Administrador/AgregarNegocio";
+import DetalleNegocio from "./Administrador/DetalleNegocio";
 
 function App() {
   return (
     <Routes>
+
+      {/* PUBLICO */}
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/admin/dashboard" element={<DashboardAdmin />} />
+
+      {/* CLIENTE */}
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/Cliente/clienteTarjetas" element={<ClienteTarjeta />} />
+
+      {/* ADMIN */}
+      <Route path="/admin/dashboard" element={<DashboardAdmin />} />
+      <Route path="/admin/negocios" element={<GestionNegocios />} />
+      <Route path="/admin/agregar" element={<AgregarNegocio />} />
+      <Route path="/admin/negocio/:id" element={<DetalleNegocio />} />
+
     </Routes>
   );
 }
