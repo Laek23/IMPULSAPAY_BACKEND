c0bb22b51b8d3bc29d9ac6cb5f7c58a37ffaf940
alexander <castillobarbudoalexander@gmail>
2026-03-04 18:03:29 -0600

    cambios miercoles

--- fluxpay/src/App.jsx
+++ fluxpay/src/App.jsx
@@ -1,53 +1,59 @@
 import { Routes, Route } from "react-router-dom";
+
 import Login from "./Login";
 import Register from "./Register";
+
+/* ================= CLIENTE ================= */
 import Dashboard from "./Cliente/Dashboard";
 import ClienteTarjeta from "./Cliente/ClienteTarjeta";
-
 import HistorialCliente from "./Cliente/HistorialCliente";
 import ClienteConfiguracion from "./Cliente/ClienteConfiguracion";
 
-
+/* ================= ADMIN ================= */
 import DashboardAdmin from "./Administrador/Dashboard";
 import GestionNegocios from "./Administrador/GestionNegocios";
 import AgregarNegocio from "./Administrador/AgregarNegocio";
 import DetalleNegocio from "./Administrador/DetalleNegocio";
+import ReportesGlobales from "./Administrador/ReportesGlobales";
+import Soporte from "./Administrador/Soporte";
 
+/* ================= NEGOCIO ================= */
 import DashboardNegocio from "./Negocio/Dashboard";
-import ProductosNegocio from "./Negocio/Productos"; 
+import ProductosNegocio from "./Negocio/Productos";
 import LayoutNegocio from "./Negocio/LayoutNegocio";
-import HistorialNegocio from './Negocio/Historial'; // Esta nueva vista
+import HistorialNegocio from "./Negocio/Historial";
 
 function App() {
- 
   return (
     <Routes>
 
-      {/* PUBLICO */}
+      {/* ================= PUBLICO ================= */}
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
-      {/* CLIENTE */}
+      {/* ================= CLIENTE ================= */}
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/Cliente/clienteTarjetas" element={<ClienteTarjeta />} />
-      <Route path="/Cliente/HistorialCliente" element={<HistorialCliente/>} />
-      <Route path="/Cliente/ClienteConfiguracion" element={<ClienteConfiguracion/>} />
+      <Route path="/cliente/clienteTarjetas" element={<ClienteTarjeta />} />
+      <Route path="/cliente/historial" element={<HistorialCliente />} />
+      <Route path="/cliente/configuracion" element={<ClienteConfiguracion />} />
 
-
-      {/* ADMIN */}
+      {/* ================= ADMIN ================= */}
       <Route path="/admin/dashboard" element={<DashboardAdmin />} />
       <Route path="/admin/negocios" element={<GestionNegocios />} />
       <Route path="/admin/agregar" element={<AgregarNegocio />} />
       <Route path="/admin/negocio/:id" element={<DetalleNegocio />} />
-  
-      {/* RUTAS DE NEGOCIO (Correctas) */}
-      <Route path="/Negocio" element={<LayoutNegocio />}>
-        {/* Usamos index para que /Negocio cargue el Dashboard por defecto */}
+      <Route path="/admin/reportes" element={<ReportesGlobales />} />
+      <Route path="/admin/soporte" element={<Soporte />} />
+
+      {/* ================= NEGOCIO ================= */}
+      <Route path="/negocio" element={<LayoutNegocio />}>
         <Route index element={<DashboardNegocio />} />
-        <Route path="Dashboard" element={<DashboardNegocio />} />
-        <Route path="Productos" element={<ProductosNegocio />} />
-        <Route path="Historial" element={<HistorialNegocio />} />
+        <Route path="dashboard" element={<DashboardNegocio />} />
+        <Route path="productos" element={<ProductosNegocio />} />
+        <Route path="historial" element={<HistorialNegocio />} />
       </Route>
+
+    
 
     </Routes>
   );
